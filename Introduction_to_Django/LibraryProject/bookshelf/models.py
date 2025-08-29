4f276a739b579c9be3323949391fac73040ec4d3
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-29 16:59:20 +0300

    create

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from bookshelf.models import Book
 
 
 class Book(models.Model):
@@ -8,5 +9,9 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+
+book = Book.object.create(
+    title="1984", author="George Orwell", publication_year=1949)
 
 # Create your models here.
