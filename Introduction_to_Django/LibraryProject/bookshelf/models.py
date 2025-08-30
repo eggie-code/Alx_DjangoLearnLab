9e27e6767feaf7e5f23c7067bacbef26d259d1f7
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-30 09:07:05 +0300

    create.md

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,17 +1,10 @@
 from django.db import models
-from bookshelf.models import Book
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_date = models.IntegerField()
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
-
-
-book = Book.object.create(
-    title="1984", author="George Orwell", publication_year=1949)
-
-# Create your models here.
