294ae72ecbb50749e489287015d0ff247ef1e38e
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-04 11:50:49 +0300

    deleted

--- django-models/LibraryProject/relationship_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
