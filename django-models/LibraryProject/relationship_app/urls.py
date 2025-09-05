6a6bf9f0cef6dc6c49917f9e13e211c836d649c0
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-05 14:54:12 +0300

    urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('librarian/', views.librarian_dashboard, name='librarian_view'),
     path('member/', views.member_dashboard, name='member_view'),
 
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book')
+    path('books/add_book/', add_book, name='add_book'),
+    path('books/<int:pk>/edit_book/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete_book/', delete_book, name='delete_book')
 ]
