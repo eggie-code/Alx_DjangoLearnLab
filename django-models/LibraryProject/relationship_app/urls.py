0ac541fd51e5bc1119b8a457156f49b1f6030e83
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-05 11:37:48 +0300

    register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
 
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register_view, name='register'),
+    path('register/', views.register_view, name='register'),
 ]
