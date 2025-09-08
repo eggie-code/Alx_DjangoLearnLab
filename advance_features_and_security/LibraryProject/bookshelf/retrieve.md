bc8a2ef24985877b1f1b2759121c8715d3fb012e
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-08 10:37:44 +0300

    advance_features_and_security

---
+++ advance_features_and_security/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,4 @@
+```python
+#retrieve
+book = Book.objects.get(title="1984")
+print(book.title, book.author, book.publication_year)
---
+++ advance_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,34 @@
+from relationship_app.models import Author, Book, Library, Librarian
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
+django.setup()
+
+
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name).first()
+if author:
+    books_by_author = Book.objects.filter(author=author)
+    print(f"Books by {author_name}:")
+    for book in books_by_author:
+        print(f" - {book.title}")
+else:
+    print(f"No author found with the name {author_name}.")
+
+library_name = "Central Library"
+library = Library.objects.get(name=library_name).first()
+if library:
+    print(f"\nBooks in {library.name}:")
+    for book in library.books.all():
+        print(f" - {book.title}")
+else:
+    print(f"No library found with the name {library_name}.")
+
+
+if library:
+    try:
+        librarian = Librarian.objects.get(library=library)
+        print(f"\nLibrarian of {library.name}: {librarian.name}")
+    except Librarian.DoesNotExist:
+        print(f"No librarian found for {library.name}.")
