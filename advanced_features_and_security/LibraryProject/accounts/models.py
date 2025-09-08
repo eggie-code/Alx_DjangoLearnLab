0f594d7e26d6190b5ba45429f10f00d0691013c2
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-08 11:36:13 +0300

    custom

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -8,23 +8,27 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError("Email is required")
         email = self.normalize_email(email)
-        user = self.model
-        (username=username, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
-    class CustomerUser(AbstractUser):
+        return self.create_user(email, password, **extra_fields) class CustomerUser(AbstractUser):
         email = models.EmailField(unique=True)
         date_of_birth = models.DateField(null=True, blank=True)
         profile_photo = models.ImageField(
             upload_to='profile_photos/', null=True, blank=True)
 
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = ['username']
         objects = BaseUserManager()
 
         def __str__(self):
