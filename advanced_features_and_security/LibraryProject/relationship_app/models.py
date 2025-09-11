d4d747e48745204d28d73c36cb7165c2a33e904a
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-11 09:34:27 +0300

    advanced_features_and_security

---
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book entry"),
+            ("can_change_book", "Can edit a book entry"),
+            ("can_delete_book", "Can delete a book entry"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+  # role-based access control
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
---
+++ advanced_features_and_security/django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView, register, login, logout
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import is_admin, is_librarian, is_member, admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
+from django.contrib.auth.decorators import user_passes_test, permission_required
+
+
+urlpatterns = [
+    # URL for the list of all books (function-based view)
+    path('books/', list_books, name='list_books'),
+
+    # URL for library details (class-based view)
+    path('library/<int:pk>/', LibraryDetailView.as_view(),
+         name='library_detail'),
+
+    # Authentication URLs
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('register/', views.register, name='register'),
+
+    # role based views
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
+
+    # URLs for custom permission-based views
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+
+]
