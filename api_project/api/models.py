89f49918f3b3bec18d1d0e8870116e05f152c024
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-15 15:28:15 +0300

    urls

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -7,5 +7,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.title
+
+def __str__(self):
+    return self.title
