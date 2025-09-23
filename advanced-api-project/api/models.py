d91f83e52c81e48398f97b1928f8e886954baa8a
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-23 14:03:15 +0300

    serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Author model stores author information
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Book model stores book information and links to Author
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+
+    # ForeignKey creates a one-to-many relationship from Author to Books
+    author = models.ForeignKey(
+        Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
