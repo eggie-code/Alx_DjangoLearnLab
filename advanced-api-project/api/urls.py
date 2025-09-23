942abe6d05d4cffc3b41aaf732e87ec30ff8e2b6
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-23 14:41:16 +0300

    urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/',
-         views.BookUpdateView.as_view(), name='book-update'),
+         views.BookUpdateView.as_view(), name='book/update'),
     path('books/<int:pk>/delete/',
-         views.BookDeleteView.as_view(), name='book-delete'),
+         views.BookDeleteView.as_view(), name='book/delete'),
 ]
