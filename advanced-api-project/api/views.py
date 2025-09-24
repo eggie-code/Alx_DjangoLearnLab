2bebe58846e9897afe95197354866086c76cf0dd
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-24 09:49:28 +0300

    views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework.permissions import generics, IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
