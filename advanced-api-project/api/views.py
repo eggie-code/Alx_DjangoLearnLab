49ed3e4f68b444e77620387038d90769fba0691a
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-24 10:13:04 +0300

    views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,8 +2,8 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, filters
-from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import DjangoFilterBackend
+from django_filters import rest_framework
 
 # List all books, support filtering, serching and ordering
 
