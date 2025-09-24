8a585aaa593d196ddb2f05fec3fb08c2e6347bd7
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-24 09:44:21 +0300

    urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,10 +4,10 @@ from .views import BookListCreateAPIView, BookRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     # the url maps to view that lists all books and allows creating a new one
-    path('books/', views.BookListCreateAPIView.as_view(), name='book-list-create'),
+    path('books/', views.BookListCreateAPIView.as_view(), name='book/create'),
 
     # URL maps to the view that retrieves, updates, or deletes a specific book.
     # The <int:pk> part captures the primary key from the URL.
     path('books/<int:pk>/delete/',
-         BookRetreiveUpdateDestroyAPUView.as_view(), name='book-detail'),
+         BookRetreiveUpdateDestroyAPUView.as_view(), name='book/delete'),
 ]
