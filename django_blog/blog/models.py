c183152e90a69deba168cd4b7c9f96f111a740a1
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-29 11:03:08 +0300

    models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, title, author, content, published_date
 
 
 class Post(models.Model):
