56ba18764d8b06db6919a6dd4009f35ddc369fa9
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-29 11:15:15 +0300

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm, UserUpdateForm
+
+# Register view
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log user in after register
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Login view
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+# Logout view
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+# Profile view
+
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
 
 # Create your views here.
