574617d2ce2e61e2d5df2e3147e19ea631c3c91f
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-30 16:23:20 +0300

    urls/settings update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -129,10 +129,20 @@ STATICFILES_DIRS = [
 
 TEMPLATES = [
     {
-        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # Or list of your templates directories
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
