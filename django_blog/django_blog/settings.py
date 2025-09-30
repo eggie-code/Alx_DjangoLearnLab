23b74b99dcc5ea9af9c1364c4a82e2f8b45fb7cd
eggie-code <fatabdi1234@gmail.com>
2025-09-30 23:35:42 +0100

    postgresql settings update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,12 +73,22 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# django_blog/settings.py
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoblog',      # Your PostgreSQL database name
+        'USER': 'bloguser',        # Your PostgreSQL username
+        'PASSWORD': 'mypassword',  # Your PostgreSQL password
+        'HOST': 'localhost',       # Or the host address
+        'PORT': '',                # Empty string means default (5432)
     }
 }
+
+
 
 
 # Password validation
