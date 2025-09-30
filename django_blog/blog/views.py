463b3338983e725a733517901cf85b6feff27175
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-30 11:10:08 +0300

    views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.models import User
 
-# Registration view
+# Register view
 
 
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log in user after registration
-            messages.success(request, 'Registration successful.')
+            login(request, user)
+            messages.success(request, "Registration successful.")
             return redirect('profile')
     else:
-        form = CustomUserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'blog/register.html', {'form': form})
 
 # Profile view
 
 
-@login_required  # User must be logged in to access
+@login_required
 def profile(request):
     if request.method == 'POST':
-        # Handle profile updates
         email = request.POST.get('email')
         request.user.email = email
         request.user.save()
-        messages.success(request, 'Profile updated.')
+        messages.success(request, "Profile updated.")
     return render(request, 'blog/profile.html')
