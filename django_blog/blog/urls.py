ab1ec6d34b0c4b86fa2553e0f3d666efae4d11ea
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-30 11:56:58 +0300

    urls.py update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
-from . import views
+from . import views(
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    register,
+    profile
+)
 from django.contrib.auth import views as auth_views
 
-urlpatterns = [
+urlpatterns = [  # authenticate
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+
+    # posts
+    path('', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
