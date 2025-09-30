24ed1d6fa9e0b22f655d4568f67150e85ea20a40
Abdulhabib junis <fatabdi1234@gmail.com>
2025-09-30 13:58:39 +0300

    urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,6 +11,7 @@ from .views import (
 )
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [  # authenticate
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
