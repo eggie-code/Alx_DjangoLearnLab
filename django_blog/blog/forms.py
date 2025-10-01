aec2fe6f8d501422ff9bfbce8a83100eae3ece71
Abdulhabib junis <fatabdi1234@gmail.com>
2025-10-01 14:27:07 +0300

    CRUD update

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,16 +1,27 @@
+from django import forms
+from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class UserRegistrationForm(UserCreationForm):
-    """
-    Customer form for user registration that includes the email field.
-    """
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'password2')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # nake email required
         self.fields['email'].required = True
+
+# creation and editing
+
+
+class PostForm(forms.ModelForm):
+    """
+    A ModelForm for the Post model, used for creating and updating posts.
+    """
+    class Meta:
+        model = Post
+        # We only let the user input title and content.
+        # 'author' will be set automatically by the view.
+        fields = ['title', 'content']
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path, include
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)  # Import the new CBVs
 
 urlpatterns = [
     # General Blog Paths
@@ -14,4 +21,20 @@ urlpatterns = [
 
     # Custom Profile Path
     path('profile/', views.profile, name='profile'),
+
+    path('', PostListView.as_view(), name='home'),  # list view
+
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # new post
+
+    path('post/<int:pk>/', PostDetailView.as_view(),
+         name='post-detail'),  # detail view
+
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(),
+         name='post-update'),  # edit post
+
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(),
+         name='post-delete'),  # delete post
+
+
+
 ]
