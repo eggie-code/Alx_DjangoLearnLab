7de5f852e92c7af5565479d4d482c7101f130f24
Abdulhabib junis <fatabdi1234@gmail.com>
2025-10-02 10:57:56 +0300

    update comments on models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User  # Import the built-in User model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Post(models.Model):
@@ -14,6 +16,46 @@ class Post(models.Model):
     # on_delete=models.CASCADE means if the User is deleted, all their posts are also deleted.
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['-published_date']
+
     def __str__(self):
         # This makes the object easily readable in the Django Admin
         return self.title
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Comment(models.Model):
+    # 'related_name' allows us to fetch comments using post.comments.all()
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Order comments by creat time, oldest first
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
