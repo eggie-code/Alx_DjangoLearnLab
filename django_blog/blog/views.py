e94c4ddb89f186d80c956fe1e54fc55666173c56
Abdulhabib junis <fatabdi1234@gmail.com>
2025-10-02 14:32:15 +0300

    updating templates html files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from .models import User
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('blog:index')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('blog:index')
+    return render(request, 'blog/login.html')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('blog:index')
+
+
+@login_required
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        user.email = request.POST['email']
+        user.save()
+    return render(request, 'blog/profile.html', {'user': user})
